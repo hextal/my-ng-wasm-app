731851e04f7924daa558b3a0d56af76516506c40
Hassan Talpur <hassan.talpur@clickdimensions.com>
2025-12-11 14:00:55 +0000

    refactor: remove TUI editor and rebrand to Photon Image Editor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,5 @@ export const routes: Routes = [
   { 
     path: 'image-editor', 
     loadComponent: () => import('./features/image-editor/image-editor.component').then(m => m.ImageEditorComponent)
-  },
-  { 
-    path: 'tui-editor', 
-    loadComponent: () => import('./features/tui-editor/tui-editor.component').then(m => m.TuiEditorComponent)
   }
 ];
